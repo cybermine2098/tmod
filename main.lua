1c0a96078390176cb31efa745842f2127ea30fd1
cybermine2098 <oliverpenneltink@gmail.com>
2025-06-04 14:27:06 -0500

    new branch, new logging.

--- main.lua
+++ main.lua
@@ -3,13 +3,6 @@
 --- MOD_ID: BFDI
 --- MOD_AUTHOR: [WusGud, Nitro, Caldox, Cyber98]
 --- MOD_DESCRIPTION: A Balatro mod themed around BFDI which adds 75 brand new jokers to the game!
-
-if not SMODS then
-    error("This mod requires Steamodded to run!")
-end
-if not BBFJ then
-    BBFJ = {}
-end
 SMODS.Atlas{
     key = 'Jokers',
     path = 'Jokers.png',
@@ -19,10 +12,11 @@ SMODS.Atlas{
 local mod_path = "" .. SMODS.current_mod.path
 -- Loading all ASSETS and whatnot
 local files = NFS.getDirectoryItems(mod_path .. "jokers")
-print("Mod path located:"..mod_path)
+print("[BDFI] Mod path located:"..mod_path)
 -- updated this a little bit to support subdirectories
 loadFromDir(mod_path .. "jokers")
 local function loadFromDir(dirname)
+    print("[BDFI] Loading [.] from directory "..dirname)
     local files = NFS.getDirectoryItems(dirname)
     local directories = NFS.getDirectoryItems(dirname, true)
     for _, file in ipairs(files) do
@@ -32,7 +26,7 @@ local function loadFromDir(dirname)
                 error(err)
             end
             local f = chunk() -- call the loaded function to get the table
-            print("Successfully loaded "..f.loc_txt.name.."!")
+            print("[BDFI] Successfully loaded "..f.loc_txt.name.."!")
             SMODS.Joker(f)
         end
     end
